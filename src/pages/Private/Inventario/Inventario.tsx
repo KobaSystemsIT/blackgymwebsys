cac720303fa3deeb88be484d0e464a8d463ad805
Victor Irad Verdeja Trujillo <viradvt@gmail.com>
2023-09-01 19:57:43 -0600

    Creada la respuesta al json del inventario
    Me tardé 2 horas pero pude 👍

--- src/pages/Private/Inventario/Inventario.tsx
+++ src/pages/Private/Inventario/Inventario.tsx
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Inventario.css';
-import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { AppStore } from "@/redux/store";
-import baseUrl from "@/services/Login/auth.service";
-import {viewInventoryData} from "@/services";
-import {viewClientsData} from "@/services/Clients/clients.service";
-
-const getInventoryUrl = baseUrl + 'getInventory';
-
+import { viewInventoryData } from "@/services";
 
 export type InventarioProps = {}
 
@@ -30,11 +24,11 @@ const Inventario: React.FC<InventarioProps> = () => {
 					setError(`Error: ${response.error}`);
 				} else {
 					// La solicitud fue exitosa, establece los datos del inventario
-					setInventoryData(response.data);
+					setInventoryData(response.inventory); // Modifiqué esto para reflejar la estructura de la respuesta
 				}
 			} catch (error) {
 				// Maneja los errores de red u otros errores aquí
-				setError(`Error de red: ${error.message}`);
+				setError(`Error de red: ${error}`);
 			}
 		};
 
@@ -48,18 +42,46 @@ const Inventario: React.FC<InventarioProps> = () => {
 				<div className=' overflow-hidden'>
 					<div className=' flex p-2 bg-gray-200 rounded-lg justify-between items-center'>
 						<h1 className='text-black text-sm'>Inventario</h1>
-						<button className='btn lg:btn-sm btn-xs bg-black text-white rounded-lg hover:text-black hover:bg-transparent'>Nuevo producto</button>\
+						<button className='btn lg:btn-sm btn-xs bg-black text-white rounded-lg hover:text-black hover:bg-transparent'>Nuevo producto</button>
 					</div>
 				</div>
 			</div>
-		</div>
-			// Hacer el map de inventoryData aquí usando map
-			{error && <p>{error}</p>}
-			<div className='inventory'>Inventory</div>
-			{/* Renderiza la tabla de inventario utilizando inventoryData en tu JSX */}
-
+			<div className='inventory'>
+				<h2>Inventario</h2>
+				{error ? (
+					<p>{error}</p>
+				) : (
+					<table>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Stock Actual</th>
+								<th>Fecha de Reorden</th>
+								<th>ID del Producto</th>
+								<th>ID del Club</th>
+								<th>Acciones</th>
+							</tr>
+						</thead>
+						<tbody>
+							{inventoryData.map((item) => (
+								<tr key={item.inventoryID}>
+									<td>{item.inventoryID}</td>
+									<td>{item.currentStock}</td>
+									<td>{item.dateReorder}</td>
+									<td>{item.productID}</td>
+									<td>{item.idClub}</td>
+									<td>
+										<button className='btn lg:btn-sm btn-xs bg-blue-500 text-white rounded-lg hover:bg-blue-700'>Editar</button>
+										<button className='btn lg:btn-sm btn-xs bg-red-500 text-white rounded-lg hover:bg-red-700'>Eliminar</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				)}
+			</div>
 		</div>
 	);
-}
+};
 
 export default Inventario;
