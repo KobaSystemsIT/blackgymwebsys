c8504b80c84f8c881f85dc5d07eaedd597388f83
Victor Irad Verdeja Trujillo <viradvt@gmail.com>
2023-09-07 15:54:15 -0600

    Update del inventario con comentarios

--- src/pages/Private/Inventario/Inventario.tsx
+++ src/pages/Private/Inventario/Inventario.tsx
@@ -1,53 +1,56 @@
+// Español:
+// Importo las dependencias necesarias.
+// Import the necessary dependencies.
 import React, { useState, useEffect } from 'react';
 import './Inventario.css';
 import { useSelector } from 'react-redux';
 import { AppStore } from "@/redux/store";
 import { viewInventoryData } from "@/services";
+import { useParams } from 'react-router-dom';
+import { faPlus, faUserPen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Inventory } from '@/models/inventory/inventory';
 
+// Defino el tipo de props para el componente Inventario.
+// Define the props type for the Inventario component.
 export type InventarioProps = {};
 
+// Defino el componente Inventario.
+// Define the Inventario component.
 const Inventario: React.FC<InventarioProps> = () => {
-  const [error, setError] = useState<string | null>(null);
-  const [inventoryData, setInventoryData] = useState<any[]>([]);
-
-  // Acceder al token desde el estado de Redux
-  const token = useSelector((store: AppStore) => store.user.token);
-
-  useEffect(() => {
-    // Define la función para obtener el inventario
-    const getInventory = async () => {
-      try {
-        const response = await viewInventoryData('1', token); // Reemplaza '1' con el ID de tu club
-				if (response.error) {
-					// La solicitud no fue exitosa, maneja el error según corresponda
-					setError(`Error: ${response.error}`);
-				} else {
-					// La solicitud fue exitosa, establece los datos del inventario
-					setInventoryData(response.data);
-				}
-			} catch (error) {
-				// Maneja los errores de red u otros errores aquí
-				setError(`Error de red: ${error}`);
-			}
-		};
-
-        if (response.error) {
-          // La solicitud no fue exitosa, maneja el error según corresponda
-          setError(`Error: ${response.error}`);
-        } else {
-          // La solicitud fue exitosa, establece los datos del inventario
-          setInventoryData(response.inventory); // Modifiqué esto para reflejar la estructura de la respuesta
-        }
-      } catch (error) {
-        // Maneja los errores de red u otros errores aquí
-        setError(`Error de red: ${error}`);
-      }
-    };
   
-    // Llama a la función para obtener el inventario
-    getInventory();
-  }, [token]); // Asegúrate de incluir el token como dependencia para que se actualice cuando cambie
+  // Obtengo el estado del usuario desde Redux.
+  // Get the user state from Redux.
+  const userState = useSelector((store: AppStore) => store.user);
+  const token = userState.token;
 
+  // Defino el estado para los datos del inventario.
+  // Define state for inventory data.
+  const [inventory, setInventoryData] = useState<Inventory[]>([]);
+  const params: any = useParams(); // Obtengo los parámetros de la URL.
+
+  // Función para obtener los datos del inventario.
+  // Function to obtain inventory data.
+  const obtainInventoryData = async () => {
+    try {
+      // Realizo la llamada a la API para obtener los datos del inventario.
+      // Make the API call to get inventory data.
+      const { inventory } = await viewInventoryData(params.idClub, token);
+      console.log(inventory); // Registro los datos del inventario en la consola.
+      setInventoryData(inventory); // Actualizo el estado con los datos del inventario.
+    } catch (error) {
+      console.error(error); // Manejo de errores en caso de problemas con la llamada a la API.
+    }
+  }
+
+  // Efecto para cargar los datos del inventario cuando se monta el componente.
+  // Effect to load inventory data when the component mounts.
+  useEffect(() => {
+    obtainInventoryData(); // Llamo a la función para obtener los datos del inventario.
+  }, []); // El segundo argumento, un arreglo vacío, asegura que este efecto solo se ejecute una vez al montar el componente.
+
+  // Renderizo la interfaz de usuario.
+  // Render the user interface.
   return (
     <div>
       <div className='grid p-2 gap-8 items-center'>
@@ -58,42 +61,42 @@ const Inventario: React.FC<InventarioProps> = () => {
           </div>
         </div>
       </div>
-      <div className='inventory'>
-        <h2>Inventario</h2>
-        {error ? (
-          <p>{error}</p>
-        ) : (
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Stock Actual</th>
-                <th>Fecha de Reorden</th>
-                <th>ID del Producto</th>
-                <th>ID del Club</th>
-                <th>Acciones</th>
+      <div className='max-h-48 overflow-auto m-2'>
+        {/* Tabla para mostrar los datos del inventario */}
+        {/* Table to display inventory data */}
+        <table className='table table-zebra table-xs table-pin-rows table-pin-cols bg-white mt-5 text-center'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Producto</th>
+              <th>Stock</th>
+              <th>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inventory.map((data) => (
+              <tr key={data.inventoryID}>
+                <td>{data.inventoryID}</td>
+                <td>{data.productName}</td>
+                <td>{data.currentStock}</td>
+                <td>{data.dateReorder ? data.dateReorder.toString().split('T')[0] : "N/A"}</td>
+                <td>
+                  {/* Botones de acciones con íconos de FontAwesome */}
+                  {/* Action buttons with FontAwesome icons */}
+                  <div className='grid grid-flow-col gap-2'>
+                    <button title='Editar Usuario'><FontAwesomeIcon icon={faUserPen} className='h-4'/></button>
+                    <button title='Gestionar Subscripción'><FontAwesomeIcon icon={faPlus} className='h-4'/></button>
+                  </div>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {inventoryData.map((item) => (
-                <tr key={item.inventoryID}>
-                  <td>{item.inventoryID}</td>
-                  <td>{item.currentStock}</td>
-                  <td>{item.dateReorder}</td>
-                  <td>{item.productID}</td>
-                  <td>{item.idClub}</td>
-                  <td>
-                    <button className='btn lg:btn-sm btn-xs bg-blue-500 text-white rounded-lg hover:bg-blue-700'>Editar</button>
-                    <button className='btn lg:btn-sm btn-xs bg-red-500 text-white rounded-lg hover:bg-red-700'>Eliminar</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
 
+// Exporto el componente Inventario.
+// Export the Inventario component.
 export default Inventario;
